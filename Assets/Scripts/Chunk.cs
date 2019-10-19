fc6c5794ced0b4656820c7688dbf42cbcf5b7fd2
Jake Chrumka <chrumka.mj@gmail.com>
2019-10-19 16:46:07 -0700

    Adding chunks

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fizz6.Building
+{
+    public class Chunk : MonoBehaviour
+    {
+        [Serializable]
+        public class Cell
+        {
+            private int Detail = 0;
+        }
+
+        private static Dictionary<Vector3i, Chunk>[] World = new Dictionary<Vector3i, Chunk>[3];
+
+        private Cell[,,] cells = null;
+
+        private string Serialize()
+        {
+            return string.Empty;
+        }
+
+        private void Deserialize(string data)
+        {
+            if (int.TryParse(data, out int size))
+            {
+                cells = new Cell[size, size, size];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Voxel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fizz6.Building
+{
+    public class Voxel : MonoBehaviour
+    {
+
+    }
+}
